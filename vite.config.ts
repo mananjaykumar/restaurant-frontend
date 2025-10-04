de6f511437e9360a8f826e2698e6e4f49abd4db2
mananjaykumar <sinharishab100@gmail.com>
2025-10-05 02:21:09 +0530

    made website crawlable to google

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-const prerender = require("vite-plugin-prerender");
 
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      staticDir: resolve(__dirname, "dist"),
-      routes: ["/"],
-      rendererOptions: {
-        renderAfterDocumentEvent: "render-event",
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     outDir: "dist",
   },
